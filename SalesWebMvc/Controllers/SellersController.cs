7bc9bf2f690d48d4db17d5fce172b6022db413a8
JoelcioFacanha <joelciomoreira@gmail.com>
2019-04-15 21:34:24 -0300

    Adicionando o SellerService e implementando o metodo FindAll do SellerService

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -3,14 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Services;
 
 namespace SalesWebMvc.Controllers
 {
     public class SellersController : Controller
     {
+        private readonly SellersService _selersService;
+
+        public SellersController(SellersService selersService)
+        {
+            _selersService = selersService;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var list = _selersService.FindAll();
+
+            return View(list);
         }
     }
 }
